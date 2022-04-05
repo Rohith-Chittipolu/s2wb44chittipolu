e4953a814d785ae32fa346e4140721e7b2dedd94
Rohith-Chittipolu <98173410+Rohith-Chittipolu@users.noreply.github.com>
2022-04-04 19:57:54 -0500

    class

--- routes/food.js
+++ routes/food.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results by food' });
+  res.render('food', { title: 'Search Results by food' });
 });
 
 module.exports = router;
